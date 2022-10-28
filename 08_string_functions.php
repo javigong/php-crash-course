25f2ae440adb359f66df006fcb9e59865c9ee193
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-10-28 11:26:52 -0700

    Update 08_string_functions.php

--- 08_string_functions.php
+++ 08_string_functions.php
@@ -48,6 +48,7 @@ if (str_ends_with($string, 'ld')) {
 
 // HTML Entities
 $string2 = '<h1>Hello World</h1>';
+echo htmlspecialchars($string2);
 echo htmlentities($string2);
 
 // Formatted Strings - useful when you have outside data
