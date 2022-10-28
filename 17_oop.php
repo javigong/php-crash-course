18f704330e1caa698d80facb19f867f6a2a73d1a
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-10-28 11:26:54 -0700

    Update 17_oop.php

--- 17_oop.php
+++ 17_oop.php
@@ -57,7 +57,7 @@ var_dump($user1);
 /* ----------- Inheritence ---------- */
 
 /*
-  Inheritence is the ability to create a new class from an existing class.
+  Inheritance is the ability to create a new class from an existing class.
   It is achieved by creating a new class that extends an existing class.
 */
 
