609896f67e43a33eb76acd576095204cc9b02b6a
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-10-28 11:26:49 -0700

    Update 01_output.php

--- 01_output.php
+++ 01_output.php
@@ -37,16 +37,19 @@ echo "Is your name O\'reilly?";
 <!-- You can output any HTML that you want within a .php file -->
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>My PHP Website</title>
 </head>
+
 <body>
   <!-- You can output PHP including variables, etc -->
   <h1>Hello <?php echo 'Brad'; ?></h1>
   <!-- You may only drop the semi-colon after a statement when the statement is followed immediately by a closing PHP tag ?>. -->
   <h1>Hello <?= 'Brad' ?></h1>
 </body>
+
 </html>
